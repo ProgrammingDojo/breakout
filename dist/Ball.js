11b67280d2f43544b121dd2b6288abb563a01d0f
lawrence415610 <34848993+lawrence415610@users.noreply.github.com>
2024-07-05 22:17:12 +1000

    Collision done

--- dist/Ball.js
+++ dist/Ball.js
@@ -2,16 +2,39 @@ import { canvas } from "./Canvas.js";
 var Ball = /** @class */ (function () {
     function Ball() {
         this.radius = 12;
-        var ballInitialLocationY = canvas.canvasHeight - this.radius;
-        var ballInitialLocationX = canvas.canvasWidth / 2;
-        this.drawBall(ballInitialLocationX, ballInitialLocationY);
+        this.x = canvas.canvasWidth / 2;
+        this.y = canvas.canvasHeight - this.radius;
+        var intervalId = this.moveBall();
     }
     Ball.prototype.drawBall = function (x, y) {
+        canvas.ctx.clearRect(0, 0, canvas.canvasWidth, canvas.canvasHeight);
         canvas.ctx.fillStyle = "blue";
         canvas.ctx.beginPath();
         canvas.ctx.arc(x, y, this.radius, 0, 2 * Math.PI);
         canvas.ctx.closePath();
         canvas.ctx.fill();
+    };
+    /**
+     * @effects change this.x, this.y based on time
+     * @returns intervalId For clear the interval
+     */
+    Ball.prototype.moveBall = function () {
+        var _this = this;
+        var moveX = 2;
+        var moveY = -2;
+        var intervalId = setInterval(function () {
+            _this.x += moveX;
+            _this.y += moveY;
+            // Collision
+            if (_this.x > canvas.canvasWidth - _this.radius || _this.x < _this.radius) {
+                moveX = -moveX;
+            }
+            if (_this.y > canvas.canvasHeight - _this.radius || _this.y < _this.radius) {
+                moveY = -moveY;
+            }
+            _this.drawBall(_this.x, _this.y);
+        }, 10);
+        return intervalId;
     };
     return Ball;
 }());
--- src/Ball.ts
+++ src/Ball.ts
@@ -2,17 +2,43 @@ import { canvas } from "./Canvas.js";
 
 export class Ball {
   private readonly radius: number = 12;
+  private x: number;
+  private y: number;
   constructor() {
-    const ballInitialLocationY = canvas.canvasHeight - this.radius;
-    const ballInitialLocationX = canvas.canvasWidth / 2;
-    this.drawBall(ballInitialLocationX, ballInitialLocationY);
+    this.x = canvas.canvasWidth / 2;
+    this.y = canvas.canvasHeight - this.radius;
+    const intervalId = this.moveBall();
   }
 
   private drawBall(x: number, y: number): void {
+    canvas.ctx.clearRect(0, 0, canvas.canvasWidth, canvas.canvasHeight);
     canvas.ctx.fillStyle = "blue";
     canvas.ctx.beginPath();
     canvas.ctx.arc(x, y, this.radius, 0, 2 * Math.PI);
     canvas.ctx.closePath();
     canvas.ctx.fill();
+  }
+
+  /**
+   * @effects change this.x, this.y based on time
+   * @returns intervalId For clear the interval
+   */
+  private moveBall(): NodeJS.Timeout {
+    let moveX = 2;
+    let moveY = -2;
+    const intervalId = setInterval(() => {
+      this.x += moveX;
+      this.y += moveY;
+      // Collision
+      if (this.x > canvas.canvasWidth - this.radius || this.x < this.radius) {
+        moveX = -moveX;
+      }
+      if (this.y > canvas.canvasHeight - this.radius || this.y < this.radius) {
+        moveY = -moveY;
+      }
+
+      this.drawBall(this.x, this.y);
+    }, 10);
+    return intervalId;
   }
 }
